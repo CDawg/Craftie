ff776c37bba4e38c9c2d35af317be144d492e15f
Porthios <cellyson78@gmail.com>
2026-07-20 16:36:40 -0500

    Updated Revision to 1.05

--- changelog.lua
+++ changelog.lua
@@ -1,4 +1,17 @@
 Craftie.CHANGELOG = [==[
+### 1.05
+* Added Request Logging / Tracking
+* Few Bugfixes for enchanting requests
+* TODO Localization
+* TODO Bottom Tab Navigation Tooltips
+* TODO Classic (Vanilla) Release
+
+### 1.04
+* Added Order Logging / Tracking
+
+### 1.03
+* Updated UI Frame to match Retail UX
+
 ### 1.02
 * Updated global libraries
 
